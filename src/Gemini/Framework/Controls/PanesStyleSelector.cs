75d6a6bc5cf6cbd0a960d137a91736399e73e134
Tim Jones <tim@timjones.tw>
2012-07-25 11:14:14 +0800

    Separate styles for documents and tools

---
+++ src/Gemini/Framework/Controls/PanesStyleSelector.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Gemini.Framework.Controls
+{
+	public class PanesStyleSelector : StyleSelector
+	{
+		public Style ToolStyle
+		{
+			get;
+			set;
+		}
+
+		public Style DocumentStyle
+		{
+			get;
+			set;
+		}
+
+		public override Style SelectStyle(object item, DependencyObject container)
+		{
+			if (item is ITool)
+				return ToolStyle;
+
+			if (item is IDocument)
+				return DocumentStyle;
+
+			return base.SelectStyle(item, container);
+		}
+	}
+}
