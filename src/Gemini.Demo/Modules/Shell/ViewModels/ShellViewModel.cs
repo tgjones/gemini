2b92ebb9fe3467e82312c14027529ce92a31799b
Tim Jones <tim@timjones.tw>
2013-05-07 23:10:05 +0800

    Provide a way to override ShellViewModel (and other built-in exports). Fixes #5.

---
+++ src/Gemini.Demo/Modules/Shell/ViewModels/ShellViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Windows;
+using Caliburn.Micro;
+using Gemini.Framework.Services;
+using Gemini.Modules.Shell.Views;
+
+namespace Gemini.Demo.Modules.Shell.ViewModels
+{
+    [Export(typeof(IShell))]
+    public class ShellViewModel : Gemini.Modules.Shell.ViewModels.ShellViewModel
+    {
+        static ShellViewModel()
+        {
+            ViewLocator.AddNamespaceMapping(typeof(ShellViewModel).Namespace, typeof(ShellView).Namespace);
+        }
+
+        public override void CanClose(Action<bool> callback)
+        {
+            Coroutine.BeginExecute(CanClose().GetEnumerator(), null, (s, e) => callback(!e.WasCancelled));
+        }
+
+        private IEnumerable<IResult> CanClose()
+        {
+            yield return new MessageBoxResult();
+        }
+
+        private class MessageBoxResult : IResult
+        {
+            public event EventHandler<ResultCompletionEventArgs> Completed;
+
+            public void Execute(ActionExecutionContext context)
+            {
+                var result = MessageBox.Show("Are you sure you want to exit?", "Confirm", MessageBoxButton.YesNo);
+                Completed(this, new ResultCompletionEventArgs { WasCancelled = (result != System.Windows.MessageBoxResult.Yes) });
+            }
+        }
+    }
+}
