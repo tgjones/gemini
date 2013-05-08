e0738f6756cf02d0bb3453b27d93ef1be17f8fa3
Tim Jones <tim@timjones.tw>
2013-05-09 00:50:10 +0800

    Add XNA demo app. Implements #8.

---
+++ src/Gemini.Demo.Xna/Modules/Startup/Module.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Composition;
+using Gemini.Framework;
+
+namespace Gemini.Demo.Xna.Modules.Startup
+{
+	[Export(typeof(IModule))]
+	public class Module : ModuleBase
+	{
+		public override void Initialize()
+		{
+			Shell.Title = "Gemini XNA Demo";
+		}
+	}
+}
