1282f91dea54a45db56cdf347bfb56dc87f061c1
Tim Jones <tim@timjones.tw>
2013-05-08 23:54:12 +0800

    Fix for priority assemblies being exported twice.
    Fixes #7.

--- src/Gemini/AppBootstrapper.cs
+++ src/Gemini/AppBootstrapper.cs
@@ -18,17 +18,24 @@ namespace Gemini
 		/// </summary>
 		protected override void Configure()
 		{
+            // Add all assemblies to AssemblySource (using a temporary DirectoryCatalog).
+            var directoryCatalog = new DirectoryCatalog(@"./");
+            AssemblySource.Instance.AddRange(
+                directoryCatalog.Parts
+                    .Select(part => ReflectionModelServices.GetPartType(part).Value.Assembly)
+                    .Where(assembly => !AssemblySource.Instance.Contains(assembly)));
+
             // Prioritise the executable assembly. This allows the client project to override exports, including IShell.
             // The client project can override SelectAssemblies to choose which assemblies are prioritised.
-		    var priorityCatalog = new AggregateCatalog(SelectAssemblies().Select(x => new AssemblyCatalog(x)));
+		    var priorityAssemblies = SelectAssemblies().ToList();
+		    var priorityCatalog = new AggregateCatalog(priorityAssemblies.Select(x => new AssemblyCatalog(x)));
 		    var priorityProvider = new CatalogExportProvider(priorityCatalog);
 
-			var directoryCatalog = new DirectoryCatalog(@"./");
-			AssemblySource.Instance.AddRange(
-				directoryCatalog.Parts
-					.Select(part => ReflectionModelServices.GetPartType(part).Value.Assembly)
-					.Where(assembly => !AssemblySource.Instance.Contains(assembly)));
-			var mainCatalog = new AggregateCatalog(AssemblySource.Instance.Select(x => new AssemblyCatalog(x)));
+            // Now get all other assemblies (excluding the priority assemblies).
+			var mainCatalog = new AggregateCatalog(
+                AssemblySource.Instance
+                    .Where(assembly => !priorityAssemblies.Contains(assembly))
+                    .Select(x => new AssemblyCatalog(x)));
 		    var mainProvider = new CatalogExportProvider(mainCatalog);
 
 			_container = new CompositionContainer(priorityProvider, mainProvider);
