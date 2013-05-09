72f4ae144bbc26e7101e219080aa6239f4c9bead
Tim Jones <tim@timjones.tw>
2013-05-09 23:50:14 +0800

    Add new Inspector module

---
+++ src/Gemini.Modules.Inspector/Views/InspectorView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Gemini.Modules.Inspector.Views
+{
+    /// <summary>
+    /// Interaction logic for InspectorView.xaml
+    /// </summary>
+    public partial class InspectorView : UserControl
+    {
+        public InspectorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
