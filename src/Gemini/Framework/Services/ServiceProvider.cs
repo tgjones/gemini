6d848d2147a0d6b720dfa44ee911694a7f521f52
Tim Jones <tim@timjones.tw>
2013-05-11 14:19:19 +0800

    Add IServiceProvider implementation, which defers to MEF

---
+++ src/Gemini/Framework/Services/ServiceProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Caliburn.Micro;
+
+namespace Gemini.Framework.Services
+{
+    /// <summary>
+    /// Used for interop with code that requires an IServiceProvider. This class
+    /// defers to the MEF container to resolve services.
+    /// </summary>
+    [Export(typeof(IServiceProvider))]
+    public class ServiceProvider : IServiceProvider
+    {
+        /// <summary>
+        /// Looks up the specified service.
+        /// </summary>
+        public object GetService(Type serviceType)
+        {
+            return IoC.GetInstance(serviceType, null);
+        }
+    }
+}
