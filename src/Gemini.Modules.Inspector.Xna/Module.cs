c1deef0914faa056ce0f0c06698350f65bb85056
Tim Jones <tim@timjones.tw>
2013-05-11 14:56:19 +0800

    Fix assembly-finding issue

---
+++ src/Gemini.Modules.Inspector.Xna/Module.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Composition;
+using Gemini.Framework;
+
+namespace Gemini.Modules.Inspector.Xna
+{
+    [Export(typeof(IModule))]
+    public class Module : ModuleBase
+    {
+        public override void Initialize()
+        {
+            
+        }
+    }
+}
