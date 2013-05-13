03705354c6930292189a4fa740e1c1ae2741160c
Tim Jones <tim@timjones.tw>
2013-05-13 23:20:02 +0800

    Add XNA color editor

---
+++ src/Gemini.Modules.Inspector.Xna/Inspectors/XnaColorEditorView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Gemini.Modules.Inspector.Xna.Inspectors
+{
+    /// <summary>
+    /// Interaction logic for XnaColorEditorView.xaml
+    /// </summary>
+    public partial class XnaColorEditorView : UserControl
+    {
+        public XnaColorEditorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Gemini.Modules.Inspector.Xna/Inspectors/XnaColorEditorViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Gemini.Modules.Inspector.Inspectors;
+using Microsoft.Xna.Framework;
+
+namespace Gemini.Modules.Inspector.Xna.Inspectors
+{
+    public class XnaColorEditorViewModel : EditorBase<Color>
+    {
+        
+    }
+}
