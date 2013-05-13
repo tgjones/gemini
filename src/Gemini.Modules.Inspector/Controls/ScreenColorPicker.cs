bd762cf8f8e41f5578f7b8f8abe7586052878d92
Tim Jones <tim@timjones.tw>
2013-05-13 23:18:48 +0800

    Add ScreenColorPicker control

---
+++ src/Gemini.Modules.Inspector/Controls/ScreenColorPicker.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Gemini.Modules.Inspector.Win32;
+using Color = System.Windows.Media.Color;
+
+namespace Gemini.Modules.Inspector.Controls
+{
+    public class ScreenColorPicker : Control
+    {
+        static ScreenColorPicker()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ScreenColorPicker),
+                new FrameworkPropertyMetadata(typeof(ScreenColorPicker)));
+        }
+
+        public event EventHandler<ColorEventArgs> ColorHovered;
+        public event EventHandler<ColorEventArgs> ColorPicked;
+
+        private Bitmap _bitmap;
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            if (!IsMouseCaptured)
+            {
+                if (_bitmap != null)
+                    _bitmap.Dispose();
+                _bitmap = NativeMethods.GetDesktop();
+
+                CaptureMouse();
+            }
+            else
+            {
+                RaiseColorPicked(GetEventArgs());
+                _bitmap.Dispose();
+                ReleaseMouseCapture();
+            }
+
+            base.OnMouseDown(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (IsMouseCaptured)
+                RaiseColorHovered(GetEventArgs());
+
+            base.OnMouseMove(e);
+        }
+
+        private ColorEventArgs GetEventArgs()
+        {
+            var cursorPosition = System.Windows.Forms.Cursor.Position;
+            var pixel = _bitmap.GetPixel(cursorPosition.X, cursorPosition.Y);
+            return new ColorEventArgs(Color.FromArgb(pixel.A, pixel.R, pixel.G, pixel.B));
+        }
+
+        private void RaiseColorHovered(ColorEventArgs e)
+        {
+            var handler = ColorHovered;
+            if (handler != null) handler(this, e);
+        }
+
+        private void RaiseColorPicked(ColorEventArgs e)
+        {
+            var handler = ColorPicked;
+            if (handler != null) handler(this, e);
+        }
+    }
+
+    public class ColorEventArgs : EventArgs
+    {
+        public Color Color { get; private set; }
+
+        public ColorEventArgs(Color color)
+        {
+            Color = color;
+        }
+    }
+}
--- src/Gemini.Modules.Inspector/Inspectors/ColorEditorView.xaml.cs
+++ src/Gemini.Modules.Inspector/Inspectors/ColorEditorView.xaml.cs
@@ -1,15 +1,23 @@
 ﻿using System.Windows.Controls;
+using Gemini.Modules.Inspector.Controls;
 
 namespace Gemini.Modules.Inspector.Inspectors
 {
-    /// <summary>
-    /// Interaction logic for ColorEditorView.xaml
-    /// </summary>
     public partial class ColorEditorView : UserControl
     {
         public ColorEditorView()
         {
             InitializeComponent();
+        }
+
+        private void OnScreenColorPickerColorHovered(object sender, ColorEventArgs e)
+        {
+            ((ColorEditorViewModel) DataContext).Value = e.Color;
+        }
+
+        private void OnScreenColorPickerColorPicked(object sender, ColorEventArgs e)
+        {
+            ((ColorEditorViewModel) DataContext).Value = e.Color;
         }
     }
 }
