427edf9ec0fa6a5df673d0d2a314c0f3d8580567
Tim Jones <tim@timjones.tw>
2013-05-13 23:20:02 +0800

    Add XNA color editor

---
+++ src/Gemini.Modules.Inspector.Xna/Converters/XnaColorToColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Microsoft.Xna.Framework;
+
+namespace Gemini.Modules.Inspector.Xna.Converters
+{
+    public class XnaColorToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var c = (Color) value;
+            return System.Windows.Media.Color.FromArgb(c.A, c.R, c.G, c.B);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var c = (System.Windows.Media.Color) value;
+            return Color.FromNonPremultiplied(c.R, c.G, c.B, c.A);
+        }
+    }
+}
