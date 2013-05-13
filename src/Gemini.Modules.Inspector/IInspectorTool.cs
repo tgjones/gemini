bcd9db2224bab2a0dac5bf9bdf7aef7cd920ba80
Tim Jones <tim@timjones.tw>
2013-05-13 23:19:20 +0800

    Add IInspectorTool.SelectedObjectChanged event

--- src/Gemini.Modules.Inspector/IInspectorTool.cs
+++ src/Gemini.Modules.Inspector/IInspectorTool.cs
@@ -1,9 +1,11 @@
-﻿using Gemini.Framework;
+﻿using System;
+using Gemini.Framework;
 
 namespace Gemini.Modules.Inspector
 {
 	public interface IInspectorTool : ITool
 	{
+	    event EventHandler SelectedObjectChanged;
         IInspectableObject SelectedObject { get; set; }
 	}
 }
