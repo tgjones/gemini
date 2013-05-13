4152907b43b58125e8196ac3e006cd3ada8053dc
Tim Jones <tim@timjones.tw>
2013-05-13 23:18:48 +0800

    Add ScreenColorPicker control

---
+++ src/Gemini.Modules.Inspector/Win32/NativeMethods.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Gemini.Modules.Inspector.Win32
+{
+    public class NativeMethods
+    {
+        [DllImport("gdi32.dll", EntryPoint = "DeleteDC")]
+        public static extern IntPtr DeleteDC(IntPtr hdc);
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        public static extern IntPtr DeleteObject(IntPtr hObject);
+
+        [DllImport("gdi32.dll", EntryPoint = "BitBlt")]
+        public static extern bool BitBlt(IntPtr hdcDest, int nXDest,
+            int nYDest, int nWidth, int nHeight, IntPtr hdcSrc,
+            int nXSrc, int nYSrc, int dwRop);
+
+        [DllImport("gdi32.dll", EntryPoint = "CreateCompatibleBitmap")]
+        public static extern IntPtr CreateCompatibleBitmap(IntPtr hdc,
+            int nWidth, int nHeight);
+
+        [DllImport("gdi32.dll", EntryPoint = "CreateCompatibleDC")]
+        public static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+
+        [DllImport("gdi32.dll", EntryPoint = "SelectObject")]
+        public static extern IntPtr SelectObject(IntPtr hdc, IntPtr hgdiobjBmp);
+
+        [DllImport("user32.dll", EntryPoint = "GetDesktopWindow")]
+        public static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32.dll", EntryPoint = "GetDC")]
+        public static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("user32.dll", EntryPoint = "GetSystemMetrics")]
+        public static extern int GetSystemMetrics(int nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "ReleaseDC")]
+        public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        public static Bitmap GetDesktop()
+        {
+            IntPtr hdcScreen = GetDC(GetDesktopWindow());
+            IntPtr hdcCompatible = CreateCompatibleDC(hdcScreen);
+
+            int screenX = GetSystemMetrics(0);
+            int screenY = GetSystemMetrics(1);
+            IntPtr hBmp = CreateCompatibleBitmap(hdcScreen, screenX, screenY);
+
+            if (hBmp != IntPtr.Zero)
+            {
+                IntPtr hOldBmp = SelectObject(hdcCompatible, hBmp);
+                BitBlt(hdcCompatible, 0, 0, screenX, screenY, hdcScreen, 0, 0, 13369376);
+
+                SelectObject(hdcCompatible, hOldBmp);
+                DeleteDC(hdcCompatible);
+                ReleaseDC(GetDesktopWindow(), hdcScreen);
+
+                Bitmap bmp = Image.FromHbitmap(hBmp);
+
+                DeleteObject(hBmp);
+                GC.Collect();
+
+                return bmp;
+            }
+
+            return null;
+        }
+    }
+}
