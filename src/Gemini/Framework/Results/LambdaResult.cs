fc782176155a64528f965ba34b638eb4e6109bac
Tim Jones <tim@timjones.tw>
2013-05-22 23:27:29 +0800

    Add LambdaResult

---
+++ src/Gemini/Framework/Results/LambdaResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Caliburn.Micro;
+
+namespace Gemini.Framework.Results
+{
+    public class LambdaResult : IResult
+    {
+        private readonly Action<ActionExecutionContext> _lambda;
+
+        public LambdaResult(Action<ActionExecutionContext> lambda)
+        {
+            _lambda = lambda;
+        }
+
+        public void Execute(ActionExecutionContext context)
+        {
+            _lambda(context);
+
+            var completedHandler = Completed;
+            if (completedHandler != null)
+                completedHandler(this, new ResultCompletionEventArgs());
+        }
+
+        public event EventHandler<ResultCompletionEventArgs> Completed;
+    }
+}
