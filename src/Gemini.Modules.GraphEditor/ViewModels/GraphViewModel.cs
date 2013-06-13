f84d241987a0a6183c932d25878f739e9042f233
Tim Jones <tim@timjones.tw>
2013-06-13 22:57:47 +0800

    Add view model methods to handle dragging

--- src/Gemini.Modules.GraphEditor/ViewModels/GraphViewModel.cs
+++ src/Gemini.Modules.GraphEditor/ViewModels/GraphViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Media;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Media;
 using Caliburn.Micro;
 using Gemini.Framework;
 
@@ -44,6 +46,56 @@ namespace Gemini.Modules.GraphEditor.ViewModels
             element.OutputConnectors.Add(new ConnectorViewModel(element) { Color = Colors.Green });
             _elements.Add(element);
             return element;
+        }
+
+        public ConnectionViewModel OnConnectionDragStarted(ConnectorViewModel sourceConnector, Point currentDragPoint)
+        {
+            // TODO: Check that source connector is an output connector.
+
+            var connection = new ConnectionViewModel
+            {
+                From = sourceConnector,
+                ToPosition = currentDragPoint
+            };
+
+            Connections.Add(connection);
+
+            return connection;
+        }
+
+        public void OnConnectionDragging(Point currentDragPoint, ConnectionViewModel connection)
+        {
+            connection.ToPosition = currentDragPoint;
+        }
+
+        public void ConnectionDragCompleted(ConnectionViewModel newConnection, ConnectorViewModel sourceConnector, ConnectorViewModel destinationConnector)
+        {
+            if (destinationConnector == null)
+            {
+                Connections.Remove(newConnection);
+                return;
+            }
+
+            // TODO: Check that destination connector is an input connector.
+
+            if (sourceConnector.Element == destinationConnector.Element)
+            {
+                Connections.Remove(newConnection);
+                return;
+            }
+
+            var existingConnection = FindConnection(sourceConnector, destinationConnector);
+            if (existingConnection != null)
+                Connections.Remove(existingConnection);
+
+            newConnection.To = destinationConnector;
+        }
+
+        private static ConnectionViewModel FindConnection(
+            ConnectorViewModel sourceConnector, 
+            ConnectorViewModel destinationConnector)
+        {
+            return sourceConnector.Connections.FirstOrDefault(connection => connection.To == destinationConnector);
         }
     }
 }
