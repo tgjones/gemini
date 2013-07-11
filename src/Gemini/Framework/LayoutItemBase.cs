e65c743a221be1aa4e3d275c7425eba1ff455b7c
Roman Novitsky <roman.novitsky@outlook.com>
2013-07-11 09:16:58 +0200

    Redone my previous commit without whitespace changes in modified files.
    Improved layout serialization: added documents support. Added layout
    items state serialization.

--- src/Gemini/Framework/LayoutItemBase.cs
+++ src/Gemini/Framework/LayoutItemBase.cs
@@ -1,9 +1,48 @@
-﻿using Caliburn.Micro;
+﻿using System;
+using System.IO;
+using System.Windows.Input;
+using Caliburn.Micro;
 
 namespace Gemini.Framework
 {
-	public abstract class LayoutItemBase : Screen
+	public abstract class LayoutItemBase : Screen, ILayoutItem
 	{
+		private readonly Guid _id = Guid.NewGuid();
 		
+		public abstract ICommand CloseCommand { get; }
+
+		public Guid Id
+		{
+			get { return _id; }
+		}
+
+		public string ContentId
+		{
+			get { return _id.ToString(); }
+		}
+
+		public virtual Uri IconSource
+		{
+			get { return null; }
+		}
+
+		private bool _isSelected;
+		public bool IsSelected
+		{
+			get { return _isSelected; }
+			set
+			{
+				_isSelected = value;
+				NotifyOfPropertyChange(() => IsSelected);
+			}
+		}
+
+		public virtual void LoadState(BinaryReader reader)
+		{
+		}
+
+		public virtual void SaveState(BinaryWriter writer)
+		{
+		}
 	}
 }
