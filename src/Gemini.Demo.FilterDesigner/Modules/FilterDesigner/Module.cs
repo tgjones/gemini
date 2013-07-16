ac9e61da8976b5e0fc4118bf4e76d48b602282b2
Tim Jones <tim@timjones.tw>
2013-07-16 20:38:29 +0800

    Update filter designer demo to support new state serialization

--- src/Gemini.Demo.FilterDesigner/Modules/FilterDesigner/Module.cs
+++ src/Gemini.Demo.FilterDesigner/Modules/FilterDesigner/Module.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Linq;
 using Caliburn.Micro;
 using Gemini.Demo.FilterDesigner.Modules.FilterDesigner.ViewModels;
 using Gemini.Framework;
+using Gemini.Framework.Results;
 using Gemini.Modules.Inspector;
+using Gemini.Modules.MainMenu.Models;
 
 namespace Gemini.Demo.FilterDesigner.Modules.FilterDesigner
 {
@@ -13,6 +16,17 @@ namespace Gemini.Demo.FilterDesigner.Modules.FilterDesigner
         public override IEnumerable<IDocument> DefaultDocuments
         {
             get { yield return new GraphViewModel(IoC.Get<IInspectorTool>()); }
+        }
+
+        public override void Initialize()
+        {
+            MainMenu.All.First(x => x.Name == "File")
+                .Children.Insert(1, new MenuItem("Open Graph", OpenGraph));
+        }
+
+        private static IEnumerable<IResult> OpenGraph()
+        {
+            yield return Show.Document(new GraphViewModel(IoC.Get<IInspectorTool>()));
         }
     }
 }
