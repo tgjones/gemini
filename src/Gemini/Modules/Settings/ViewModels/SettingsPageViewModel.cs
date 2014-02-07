1e74bac435bcf8854a5928d2d9c9da5a57984516
Roman Novitsky <roman.novitsky@outlook.com>
2014-02-07 09:28:00 +0100

    Basic settings view implementation.

---
+++ src/Gemini/Modules/Settings/ViewModels/SettingsPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Gemini.Modules.Settings.ViewModels
+{
+    public class SettingsPageViewModel
+    {
+        public SettingsPageViewModel()
+        {
+            Children = new List<SettingsPageViewModel>();
+        }
+
+        public string Name { get; set; }
+        public ISettingsEditor Editor { get; set; }
+        public List<SettingsPageViewModel> Children { get; private set; }
+    }
+}
--- src/Gemini/Modules/Settings/ViewModels/SettingsViewModel.cs
+++ src/Gemini/Modules/Settings/ViewModels/SettingsViewModel.cs
@@ -1,11 +1,95 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Windows.Input;
+using Caliburn.Micro;
 using Gemini.Framework;
 
 namespace Gemini.Modules.Settings.ViewModels
 {
-	[Export(typeof(SettingsViewModel))]
-	public class SettingsViewModel : WindowBase
-	{
+    [Export(typeof (SettingsViewModel))]
+    public class SettingsViewModel : WindowBase
+    {
+        private IEnumerable<ISettingsEditor> _settingsEditors;
 
-	}
+        public SettingsViewModel()
+        {
+            CancelCommand = new RelayCommand(o => TryClose());
+            OkCommand = new RelayCommand(SaveChanges);
+
+            DisplayName = "Options";
+        }
+
+        public List<SettingsPageViewModel> Pages { get; private set; }
+        public ICommand CancelCommand { get; private set; }
+        public ICommand OkCommand { get; private set; }
+
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+
+            var pages = new List<SettingsPageViewModel>();
+            _settingsEditors = IoC.GetAll<ISettingsEditor>();
+
+            foreach (ISettingsEditor settingsEditor in _settingsEditors)
+            {
+                List<SettingsPageViewModel> parentCollection = GetParentCollection(settingsEditor, pages);
+
+                SettingsPageViewModel existingPage =
+                    parentCollection.FirstOrDefault(m => m.Name == settingsEditor.SettingsPageName);
+
+                if (existingPage != null && existingPage.Editor == null)
+                {
+                    existingPage.Editor = settingsEditor;
+                }
+                else
+                {
+                    parentCollection.Add(new SettingsPageViewModel
+                    {
+                        Editor = settingsEditor,
+                        Name = settingsEditor.SettingsPageName
+                    });
+                }
+            }
+
+            Pages = pages;
+        }
+
+        private List<SettingsPageViewModel> GetParentCollection(ISettingsEditor settingsEditor,
+            List<SettingsPageViewModel> pages)
+        {
+            if (string.IsNullOrEmpty(settingsEditor.SettingsPagePath))
+            {
+                return pages;
+            }
+
+            string[] path = settingsEditor.SettingsPagePath.Split(new[] {'\\'}, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string pathElement in path)
+            {
+                SettingsPageViewModel page = pages.FirstOrDefault(s => s.Name == pathElement);
+
+                if (page == null)
+                {
+                    page = new SettingsPageViewModel {Name = pathElement};
+                    pages.Add(page);
+                }
+
+                pages = page.Children;
+            }
+
+            return pages;
+        }
+
+        private void SaveChanges(object obj)
+        {
+            foreach (ISettingsEditor settingsEditor in _settingsEditors)
+            {
+                settingsEditor.ApplyChanges();
+            }
+
+            TryClose();
+        }
+    }
 }
