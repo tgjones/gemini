c8edd91de13f3eb29fa9079fbd1e39db48acc4ff
Tim Jones <tim@timjones.tw>
2014-05-04 18:10:01 +0800

    Lazy-load exports to prevent multiple initialization.
    Fixes #36.

--- src/Gemini/AppBootstrapper.cs
+++ src/Gemini/AppBootstrapper.cs
@@ -60,10 +60,10 @@ namespace Gemini
 		protected override object GetInstance(Type serviceType, string key)
 		{
 			string contract = string.IsNullOrEmpty(key) ? AttributedModelServices.GetContractName(serviceType) : key;
-			var exports = Container.GetExportedValues<object>(contract);
+			var exports = Container.GetExports<object>(contract);
 
-			if (exports.Count() > 0)
-				return exports.First();
+			if (exports.Any())
+				return exports.First().Value;
 
 			throw new Exception(string.Format("Could not locate any instances of contract {0}.", contract));
 		}
