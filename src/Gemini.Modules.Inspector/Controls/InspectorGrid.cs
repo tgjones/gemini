67013246b9dfc3a8071d6285428e475bc2221177
Tim Jones <tim@timjones.tw>
2014-12-08 08:40:13 +0800

    Use [Category] attribute to group inspector editors.
    Only used for InspectorBuilder.WithObjectProperties; you can still
    completely customise the inspector by adding editors manually. Fixes
    #93.

---
+++ src/Gemini.Modules.Inspector/Controls/InspectorGrid.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+
+namespace Gemini.Modules.Inspector.Controls
+{
+    public class InspectorGrid
+    {
+        public static event EventHandler PropertyNameColumnWidthChanged;
+        public static event EventHandler PropertyValueColumnWidthChanged;
+
+        private static GridLength _propertyNameColumnWidth = new GridLength(1, GridUnitType.Star);
+        public static GridLength PropertyNameColumnWidth
+        {
+            get { return _propertyNameColumnWidth; }
+            set
+            {
+                _propertyNameColumnWidth = value;
+                var handler = PropertyNameColumnWidthChanged;
+                if (handler != null)
+                    handler(null, EventArgs.Empty);
+            }
+        }
+
+
+        private static GridLength _propertyValueColumnWidth = new GridLength(1.5, GridUnitType.Star);
+        public static GridLength PropertyValueColumnWidth
+        {
+            get { return _propertyValueColumnWidth; }
+            set
+            {
+                _propertyValueColumnWidth = value;
+                var handler = PropertyValueColumnWidthChanged;
+                if (handler != null)
+                    handler(null, EventArgs.Empty);
+            }
+        }
+    }
+}
