93b21b7a095eabec43bbeadf9c92affdefc1f7a3
Tim Jones <tim@timjones.tw>
2014-12-13 21:44:54 +0800

    Persist expand / collapse state for inspector groups
    (somewhat experimental)

--- src/Gemini.Modules.Inspector/Inspectors/CollapsibleGroupViewModel.cs
+++ src/Gemini.Modules.Inspector/Inspectors/CollapsibleGroupViewModel.cs
@@ -4,6 +4,8 @@ namespace Gemini.Modules.Inspector.Inspectors
 {
     public class CollapsibleGroupViewModel : InspectorBase
     {
+        private static readonly Dictionary<string, bool> PersistedExpandCollapseStates = new Dictionary<string, bool>();
+
         private readonly string _name;
         private readonly IEnumerable<IInspector> _children;
 
@@ -22,10 +24,25 @@ namespace Gemini.Modules.Inspector.Inspectors
             get { return _children; }
         }
 
+        private bool _isExpanded;
+        public bool IsExpanded
+        {
+            get { return _isExpanded; }
+            set
+            {
+                _isExpanded = value;
+                PersistedExpandCollapseStates[_name] = value; // TODO: Key should be full path to this group, not just the name.
+                NotifyOfPropertyChange();
+            }
+        }
+
         public CollapsibleGroupViewModel(string name, IEnumerable<IInspector> children)
         {
             _name = name;
             _children = children;
+
+            if (!PersistedExpandCollapseStates.TryGetValue(_name, out _isExpanded))
+                _isExpanded = true;
         }
     }
 }
