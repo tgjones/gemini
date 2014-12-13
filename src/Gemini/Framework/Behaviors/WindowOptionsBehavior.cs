53dc4354350e0474aa9c6e51a3d54df9bd6ff853
Tim Jones <tim@timjones.tw>
2014-12-13 10:19:12 +0800

    Use standard window style for settings dialog
    (no minimize or maximize button, no icon)

---
+++ src/Gemini/Framework/Behaviors/WindowOptionsBehavior.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interactivity;
+using System.Windows.Interop;
+using Gemini.Framework.Win32;
+
+namespace Gemini.Framework.Behaviors
+{
+    public class WindowOptionsBehavior : Behavior<Window>
+    {
+        public static readonly DependencyProperty ShowIconProperty = DependencyProperty.Register(
+            "ShowIcon", typeof(bool), typeof(WindowOptionsBehavior), 
+            new PropertyMetadata(true, OnWindowOptionChanged));
+
+        public bool ShowIcon
+        {
+            get { return (bool) GetValue(ShowIconProperty); }
+            set { SetValue(ShowIconProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowMinimizeBoxProperty = DependencyProperty.Register(
+            "ShowMinimizeBox", typeof(bool), typeof(WindowOptionsBehavior),
+            new PropertyMetadata(true, OnWindowOptionChanged));
+
+        public bool ShowMinimizeBox
+        {
+            get { return (bool) GetValue(ShowMinimizeBoxProperty); }
+            set { SetValue(ShowMinimizeBoxProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowMaximizeBoxProperty = DependencyProperty.Register(
+            "ShowMaximizeBox", typeof(bool), typeof(WindowOptionsBehavior),
+            new PropertyMetadata(true, OnWindowOptionChanged));
+
+        public bool ShowMaximizeBox
+        {
+            get { return (bool) GetValue(ShowMaximizeBoxProperty); }
+            set { SetValue(ShowMaximizeBoxProperty, value); }
+        }
+
+        private static void OnWindowOptionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((WindowOptionsBehavior) d).UpdateWindowStyle();
+        }
+
+        private void UpdateWindowStyle()
+        {
+            if (AssociatedObject == null)
+                return;
+
+            var handle = new WindowInteropHelper(AssociatedObject).Handle;
+
+            var windowStyle = NativeMethods.GetWindowLong(handle, NativeMethods.GWL_STYLE);
+
+            if (ShowMinimizeBox)
+                windowStyle |= NativeMethods.WS_MINIMIZEBOX;
+            else
+                windowStyle &= ~NativeMethods.WS_MINIMIZEBOX;
+
+            if (ShowMaximizeBox)
+                windowStyle |= NativeMethods.WS_MAXIMIZEBOX;
+            else
+                windowStyle &= ~NativeMethods.WS_MAXIMIZEBOX;
+
+            NativeMethods.SetWindowLong(handle, NativeMethods.GWL_STYLE, windowStyle);
+
+            if (ShowIcon)
+            {
+                // TODO
+            }
+            else
+            {
+                var exWindowStyle = NativeMethods.GetWindowLong(handle, NativeMethods.GWL_EXSTYLE);
+                NativeMethods.SetWindowLong(handle, NativeMethods.GWL_EXSTYLE,
+                    exWindowStyle | NativeMethods.WS_EX_DLGMODALFRAME);
+
+                NativeMethods.SetWindowPos(handle, IntPtr.Zero, 0, 0, 0, 0,
+                    NativeMethods.SWP_NOMOVE | NativeMethods.SWP_NOSIZE | NativeMethods.SWP_NOZORDER | NativeMethods.SWP_FRAMECHANGED);
+
+                NativeMethods.SendMessage(handle, NativeMethods.WM_SETICON, IntPtr.Zero, IntPtr.Zero);
+            }
+        }
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.SourceInitialized += OnSourceInitialized;
+            base.OnAttached();
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.SourceInitialized -= OnSourceInitialized;
+            base.OnDetaching();
+        }
+
+        private void OnSourceInitialized(object sender, EventArgs e)
+        {
+            UpdateWindowStyle();
+        }
+    }
+}
---
+++ src/Gemini/Framework/Win32/NativeMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Gemini.Framework.Win32
+{
+    internal static class NativeMethods
+    {
+        public const int GWL_STYLE = -16;
+        public const int GWL_EXSTYLE = -20;
+
+        public const int WS_MAXIMIZEBOX = 0x10000;
+        public const int WS_MINIMIZEBOX = 0x20000;
+
+        public const int WS_EX_DLGMODALFRAME = 0x00000001;
+
+        public const int SWP_NOSIZE = 0x0001;
+        public const int SWP_NOMOVE = 0x0002;
+        public const int SWP_NOZORDER = 0x0004;
+        public const int SWP_FRAMECHANGED = 0x0020;
+
+        public const uint WM_SETICON = 0x0080;
+
+        [DllImport("user32.dll")]
+        public extern static int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        public extern static int SetWindowLong(IntPtr hwnd, int index, int value);
+
+        [DllImport("user32.dll")]
+        public extern static bool SetWindowPos(IntPtr hwnd, IntPtr hwndInsertAfter, 
+            int x, int y, int width, int height, uint flags);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hwnd, uint msg, 
+            IntPtr wParam, IntPtr lParam);
+    }
+}
