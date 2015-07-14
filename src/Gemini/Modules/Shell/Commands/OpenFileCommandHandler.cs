31a65e6d98d8fc2416b3c99e188554bc4a61cb2e
Tim Jones <tim@timjones.tw>
2015-07-14 18:17:12 +0800

    Add file filter for opening files

--- src/Gemini/Modules/Shell/Commands/OpenFileCommandHandler.cs
+++ src/Gemini/Modules/Shell/Commands/OpenFileCommandHandler.cs
@@ -14,16 +14,25 @@ namespace Gemini.Modules.Shell.Commands
     public class OpenFileCommandHandler : CommandHandlerBase<OpenFileCommandDefinition>
     {
         private readonly IShell _shell;
+        private readonly IEditorProvider[] _editorProviders;
 
         [ImportingConstructor]
-        public OpenFileCommandHandler(IShell shell)
+        public OpenFileCommandHandler(IShell shell, [ImportMany] IEditorProvider[] editorProviders)
         {
             _shell = shell;
+            _editorProviders = editorProviders;
         }
 
         public override async Task Run(Command command)
         {
             var dialog = new OpenFileDialog();
+
+            dialog.Filter = "All Supported Files|" + string.Join(";", _editorProviders
+                .SelectMany(x => x.FileTypes).Select(x => "*" + x.FileExtension));
+
+            dialog.Filter += "|" + string.Join("|", _editorProviders
+                .SelectMany(x => x.FileTypes)
+                .Select(x => x.Name + "|*" + x.FileExtension));
 
             if (dialog.ShowDialog() == true)
                 _shell.OpenDocument(await GetEditor(dialog.FileName));
