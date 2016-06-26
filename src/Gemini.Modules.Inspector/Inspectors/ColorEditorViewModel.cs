bee81bb42549b1c004e9991ae44c5866bd83c6aa
Axel Gembe <axel@gembe.net>
2016-06-26 09:17:22 +0700

    Color Picker: Add binding for UsingAlphaChannel
    Signed-off-by: Axel Gembe <axel@gembe.net>

--- src/Gemini.Modules.Inspector/Inspectors/ColorEditorViewModel.cs
+++ src/Gemini.Modules.Inspector/Inspectors/ColorEditorViewModel.cs
@@ -4,6 +4,21 @@ namespace Gemini.Modules.Inspector.Inspectors
 {
     public class ColorEditorViewModel : EditorBase<Color>, ILabelledInspector
     {
-        
+        private bool _usingAlphaChannel = true;
+
+        public bool UsingAlphaChannel
+        {
+            get { return _usingAlphaChannel; }
+
+            set
+            {
+                if (_usingAlphaChannel == value)
+                    return;
+
+                _usingAlphaChannel = value;
+
+                NotifyOfPropertyChange(() => UsingAlphaChannel);
+            }
+        }
     }
 }
