5b96c4131171be79b9a69bb99156ec248913e4ea
diluculo <diluculo@gmail.com>
2016-08-25 15:56:09 +0900

    Add save prompt and modify CodeEditor and TextEditor

--- src/Gemini/Framework/PersistedDocument.cs
+++ src/Gemini/Framework/PersistedDocument.cs
@@ -1,6 +1,10 @@
+using Caliburn.Micro;
+using Gemini.Framework.Services;
+using Gemini.Properties;
+using Microsoft.Win32;
 using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Gemini.Framework
 {
@@ -26,10 +30,78 @@ namespace Gemini.Framework
             }
         }
 
+        // ShouldReopenOnStart, SaveState and LoadState are default methods of PersistedDocument.
+        public override bool ShouldReopenOnStart
+        {
+            get { return (FilePath != null); }  // if FilePath is null, SaveState() will generate an NullExceptionError
+        }
+                
+        public override void SaveState(BinaryWriter writer)
+        {
+            writer.Write(FilePath);
+        }
+
+        public override async void LoadState(BinaryReader reader)
+        {
+            await Load(reader.ReadString());
+        }
+
         public override Task<bool> CanCloseAsync(CancellationToken cancellationToken)
         {
-            // TODO: Show save prompt.
-            return Task.FromResult(!IsDirty);
+            if (IsDirty)
+            {
+                // Show save prompt.  
+                // Note that CanClose method of Demo ShellViewModel blocks this. 
+                string title = IoC.Get<IMainWindow>().Title;
+                string fileName = Path.GetFileNameWithoutExtension(FileName);
+                string fileExtension = Path.GetExtension(FileName);
+                var fileType = IoC.GetAll<IEditorProvider>()
+                                  .SelectMany(x => x.FileTypes)
+                                  .SingleOrDefault(x => x.FileExtension == fileExtension);
+
+                string message = string.Format(Resources.SaveChangesBeforeClosingMessage, fileType.Name, fileName);
+                var result = MessageBox.Show(message, title, MessageBoxButton.YesNoCancel, MessageBoxImage.Warning);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    if (IsNew)
+                    {
+                        // Ask new file path.
+                        var filter = string.Empty;
+                        if (fileType != null)
+                            filter = fileType.Name + "|*" + fileType.FileExtension + "|";
+                        filter += Properties.Resources.AllFiles + "|*.*";
+
+                        var dialog = new SaveFileDialog() { FileName = this.FileName, Filter = filter };
+                        if (dialog.ShowDialog() == true)
+                        {
+                            // Save file.
+                            await Save(dialog.FileName);
+
+                            // Add to recent files. Temporally, commented out.
+                            //IShell _shell = IoC.Get<IShell>();
+                            //_shell.RecentFiles.Update(dialog.FileName);
+                        }
+                        else
+                        {
+                            callback(false);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        // Save file.
+                        await Save(FilePath);
+                    }
+                }
+                else if (result == MessageBoxResult.Cancel)
+                {
+                    callback(false);
+                    return;
+                }
+            }
+
+            callback(true);
         }
 
         private void UpdateDisplayName()
