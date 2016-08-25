ac522c99a726fedf309087ada665b5c9f9048d9f
diluculo <diluculo@gmail.com>
2016-08-25 16:37:55 +0900

    Add SaveAllCommand

---
+++ src/Gemini/Modules/Shell/Commands/SaveAllFilesCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using Gemini.Framework;
+using Gemini.Framework.Commands;
+using Gemini.Framework.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+namespace Gemini.Modules.Shell.Commands
+{
+    [CommandHandler]
+    public class SaveAllFilesCommandHandler : CommandHandlerBase<SaveAllFilesCommandDefinition>
+    {
+        private readonly IShell _shell;
+
+        [ImportingConstructor]
+        public SaveAllFilesCommandHandler(IShell shell)
+        {
+            _shell = shell;
+        }
+
+        public override async Task Run(Command command)
+        {
+            var tasks = new List<Task<Tuple<IPersistedDocument, bool>>>();
+
+            foreach (var document in _shell.Documents)
+            {
+                var persistedDocument = document as IPersistedDocument;
+                if (persistedDocument == null) continue;
+
+                // skip if the file is new
+                if (!persistedDocument.IsNew)
+                {
+                    await persistedDocument.Save(persistedDocument.FilePath);
+                }
+            }
+
+            // TODO: display "Item(s) saved" in statusbar
+        }
+    }
+}
