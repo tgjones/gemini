1226693b3f3e615fe6fac0ea02e2b3fe6fbeb5c9
ReMinoer <reminoer@gmail.com>
2020-12-02 21:07:31 +0100

    Support icon for editor file types

--- src/Gemini/Framework/Services/EditorFileType.cs
+++ src/Gemini/Framework/Services/EditorFileType.cs
@@ -1,14 +1,18 @@
-﻿namespace Gemini.Framework.Services
+using System;
+
+namespace Gemini.Framework.Services
 {
     public class EditorFileType
     {
         public string Name { get; set; }
         public string FileExtension { get; set; }
+        public Uri IconSource { get; set; }
 
-        public EditorFileType(string name, string fileExtension)
+        public EditorFileType(string name, string fileExtension, Uri iconSource = null)
         {
             Name = name;
             FileExtension = fileExtension;
+            IconSource = iconSource;
         }
 
         public EditorFileType()
